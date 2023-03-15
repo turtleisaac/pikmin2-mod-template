ac1c397a12999e22ba85bf20f7ff66e4e21a02eb
turtleisaac <turtleisaac1@gmail.com>
2023-03-15 17:52:11 -0400

    fixing calls obtaining enemy name

--- src/moddingU/disasterEnemyMgr.cpp
+++ src/moddingU/disasterEnemyMgr.cpp
@@ -81,7 +81,7 @@ EnemyMgrBase * createDisasterEnemyMgr(u8 type, int enemyID, int limit)
 {
 	// int limit = objLimit;
 	EnemyInfoFunc::getEnemyInfo(enemyID, 0xFFFF);
-	char* name = GeneralEnemyMgr::getEnemyName(enemyID, 0xFFFF);
+	char* name = EnemyInfoFunc::getEnemyName(enemyID, 0xFFFF)
 	sys->heapStatusStart(name, nullptr);
 
 	EnemyMgrBase* mgr;
@@ -374,7 +374,7 @@ EnemyMgrBase * createDisasterEnemyMgr(u8 type, int enemyID, int limit)
 
 	mgr->alloc();
 //	mEnemyMgrNode.add(new EnemyMgrNode(enemyID, name, mgr));
-	add(mgr);
+//	add(mgr);
 	sys->heapStatusEnd(name);
 	return mgr;
 }
