863125b0634fe9392cdde095a29bffa708d4ec0b
turtleisaac <turtleisaac1@gmail.com>
2023-03-14 22:31:04 -0400

    set up camera hooking stuff in theory for repeating function

---
+++ src/moddingU/cameraHook.cpp
@@ -0,0 +1,27 @@
+#include "types.h"
+#include "Dolphin/rand.h"
+#include "Game/VsGame.h"
+#include "efx2d/Arg.h"
+#include "efx2d/T2DSprayset.h"
+#include "Light.h"
+#include "JSystem/JKernel/JKRArchive.h"
+#include "PSSystem/PSSystemIF.h"
+#include "Game/Entities/ItemOnyon.h"
+#include "Game/Stickers.h"
+#include "Game/pelletMgr.h"
+#include "Game/GameConfig.h"
+#include "nans.h"
+#include "Modding/mod.h"
+
+struct PlayCamera;
+
+namespace Game {
+
+void cameraHook(PlayCamera& player_camera)
+{
+	disasterGeneral();
+}
+
+} // namespace Game
+
+
